7bd891dea62a6e7e13e75860d35188171fb400b5
zhiwei.xu@ali <zhiwei.xu@ali>
2019-08-05 17:29:38 +0800

    v2.0

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,5 +1,12 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+ Author: zhiwei.xu
+ Date: 2019-08-04 18:05:12
+ LastEditors: zhiwei.xu
+ LastEditTime: 2019-08-05 17:12:51
+'''
+
 from __future__ import unicode_literals
 
 AUTHOR = 'Chihwei-Hsu'
@@ -12,6 +19,7 @@ TIMEZONE = 'Asia/Shanghai'
 
 DEFAULT_LANG = 'zh_cn'
 
+THEME = "elegant"
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -33,7 +41,13 @@ DEFAULT_PAGINATION = 10
 
 PORT = 9999
 
-# BIND = '0.0.0.0'
+BIND = '0.0.0.0'
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
+# IPYNB_USE_METACELL = True
+
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATHS = ['./plugins']
+PLUGINS = ['ipynb.markup']
